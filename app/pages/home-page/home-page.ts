92a02a9972e5961f15ef8f3843d164394f69e5c1
geoHeil <e1225063@student.tuwien.ac.at>
2016-06-15 13:49:19 +0200

    add jsonix + reproduce error

--- app/pages/home-page/home-page.ts
+++ app/pages/home-page/home-page.ts
@@ -1,15 +1,20 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {ScientificFactsPage} from '../scientific-facts-page/scientific-facts-page';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {ScientificFactsPage} from "../scientific-facts-page/scientific-facts-page";
+import {Jsonix} from "../../node_modules/jsonix/jsonix";
 
 @Component({
-  templateUrl: 'build/pages/home-page/home-page.html'
+    templateUrl: 'build/pages/home-page/home-page.html'
 })
 export class HomePage {
-  constructor(private _navController: NavController) {
-  }
+    
+    constructor(private _navController:NavController) {
 
-  goToFactsPage(){
-    this._navController.push(ScientificFactsPage);
-  }
+        var context = new Jsonix.Context(["XSD/generated/model"]);
+        var unmarshaller = context.createUnmarshaller();
+    }
+
+    goToFactsPage() {
+        this._navController.push(ScientificFactsPage);
+    }
 }
